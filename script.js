bd9f703cc12c3846b9ca51b48c6211c0a05a81a1
Khemchandra Persadsingh <khemchandra@gmail.com>
2022-07-10 16:11:45 -0400

    display the interest only in results

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ function compute()
         const year = new Date().getFullYear() + parseInt(years);
 
         //display results of calculation
-        results = `If you deposit <span class="highlight">${principal}</span>,<br/>at an interest rate of <span class="highlight">${rate}%</span>.<br/>You will receive an amount of <span class="highlight">${principal + interest}</span>,<br/>in the year <span class="highlight">${year}</span>.`;
+        results = `If you deposit <span class="highlight">${principal}</span>,<br/>at an interest rate of <span class="highlight">${rate}%</span>.<br/>You will receive an amount of <span class="highlight">${interest}</span>,<br/>in the year <span class="highlight">${year}</span>.`;
         document.getElementById('result').innerHTML = results;
     }
 }
