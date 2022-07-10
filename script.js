3b3f6d0c1e3946b213865bcdad6af59943c7d973
Khemchandra Persadsingh <khemchandra@gmail.com>
2022-07-10 14:21:12 -0400

    add functions to add on change listener to rate slider and validate principal amount

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
+window.onload = function() {
+    // attach event handlers after the DOM has loaded
+
+    //rate slider input, on change value do the following
+    // - update the rate value displayed to the rate selected
+    // - recompute the interest
+    const rateElement = document.getElementById('rate');
+    rateElement.addEventListener('change', (event) => {
+        const rateDisplay = document.getElementById('rate_val');
+        rateDisplay.textContent = `${event.target.value}%`;
+    });
+}
+
+function isValidInput() {
+    //principal input, ensure value entered is positive number
+    const principalElement = document.getElementById('principal');
+    if (isNaN(parseFloat(principalElement.value))) {
+        principalElement.focus();
+        alert('Enter a positive number');
+        return false;
+    }
+    else if (parseFloat(principalElement.value) <= 0) {
+        principalElement.focus();
+        alert('Enter a positive number');
+        return false;
+    }
+
+    return true;
+}
+
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    if (isValidInput()) {
+        p = document.getElementById("principal").value;
+    }
 }
         
